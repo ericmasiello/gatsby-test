a31c58c170b2bec2b2cfb91a9dfd577e17307985
Eric Masiello <eric.j.masiello@gmail.com>
2018-06-13 21:21:46 -0400

    add hiring form

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,6 +12,21 @@ class AboutPage extends React.Component {
         <h1>Learn about my website</h1>
         <p onClick={this.increment}>lorem ipsum {this.state.count}</p>
         <Link to="/">Go back to the homepage</Link>
+
+        <form name="hiring" method="POST" netlify>
+          <p>
+            <label>Your Name: <input type="text" name="name" /></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <label>Why do you want to work for us?: <textarea name="why"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
       </div>
     );
   }
