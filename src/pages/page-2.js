10cc76f3d62c71df4eb90bef303fcbe04057afa8
Eric Masiello <eric.j.masiello@gmail.com>
2018-06-13 21:27:12 -0400

    another contact

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -5,6 +5,17 @@ const SecondPage = () => (
   <div>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <form name="contact2" netlify>
+      <p>
+        <label>Name <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Email <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )
